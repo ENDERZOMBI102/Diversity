51ea0c4cebc8085880f7ea0f72abc29769a2abe1
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-09-15 17:49:23 +0200

    more gradle work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
-@file:Suppress("PropertyName", "UnstableApiUsage", "SpellCheckingInspection")
+@file:Suppress("SpellCheckingInspection", "UnstableApiUsage")
+
+import net.fabricmc.loom.api.mappings.layered.spec.LayeredMappingSpecBuilder
+import net.fabricmc.loom.task.RemapSourcesJarTask
+
 plugins {
 	id("org.quiltmc.loom").version( "0.12.+" )
 	java
@@ -8,13 +12,8 @@ val group: String by project
 val version: String by project
 val archivesBaseName: String by project
 
-val minecraft_version = libs.versions.minecraft.get()
-val loader_version = libs.versions.loader.get()
-val mappings = libs.versions.mappings.get()
-
 allprojects {
 	repositories {
-		mavenLocal()
 		mavenCentral()
 		maven( "https://jitpack.io" )
 		maven( "https://maven.gegy.dev" )
@@ -33,6 +32,17 @@ allprojects {
 fun bundle( proj: String, bundleName: String ) =
 	libs.findBundle( "${proj.toLowerCase()}.$bundleName" ).get()
 
+/**
+ * Utility function that retrieves a version from the version catalog
+ */
+fun version( name: String ) = libs.findVersion( name ).get().displayName
+
+/**
+ * Basically to make the mappings an oneliner.
+ */
+fun LayeredMappingSpecBuilder.quilt( minecraftVersion: String, mappingsVersion: String ) =
+	addLayer( quiltMappings.mappings( "org.quiltmc:quilt-mappings:$minecraftVersion+build.$mappingsVersion:v2" ) )
+
 subprojects {
 	apply( plugin="org.quiltmc.loom" )
 
@@ -41,10 +51,8 @@ subprojects {
 	loom.runConfigs["server"].runDir = "../run"
 
 	dependencies {
-		minecraft( "com.mojang:minecraft:$minecraft_version" )
-		mappings( loom.layered {
-			addLayer( quiltMappings.mappings( "org.quiltmc:quilt-mappings:$minecraft_version+build.$mappings:v2" ) )
-		})
+		minecraft( "com.mojang:minecraft:${version("minecraft")}" )
+		mappings( loom.layered { quilt( version("minecraft"), version("mappings") ) } )
 
 		// Common dependencies
 		implementation( bundle( "common", "implementation" ) )
@@ -63,18 +71,18 @@ subprojects {
 	}
 
 	tasks.withType<ProcessResources>().configureEach {
-		inputs.property( "version", version )
-		inputs.property( "group", "com.enderzombi102.diversity" )
-		inputs.property( "loader_version", loader_version )
-		inputs.property( "minecraft_version", minecraft_version )
+		inputs.property( "version"          , version )
+		inputs.property( "group"            , "com.enderzombi102.diversity" )
+		inputs.property( "loader_version"   , rootProject.libs.versions.loader.get() )
+		inputs.property( "minecraft_version", libs.versions.minecraft.get() )
 		filteringCharset = "UTF-8"
 
 		filesMatching( "quilt.mod.json" ) {
 			expand(
 				Pair( "version"          , version ),
 				Pair( "group"            , "com.enderzombi102.diversity" ),
-				Pair( "loader_version"   , loader_version ),
-				Pair( "minecraft_version", minecraft_version ),
+				Pair( "loader_version"   , rootProject.libs.versions.loader.get() ),
+				Pair( "minecraft_version", libs.versions.minecraft.get() ),
 			)
 		}
 	}
@@ -91,6 +99,19 @@ subprojects {
 		withSourcesJar()
 	}
 
+	tasks.withType<AbstractArchiveTask>().configureEach {
+		archiveBaseName.set( project.name.toLowerCase() )
+		destinationDirectory.set(
+			rootProject.buildDir.resolve(
+				when ( archiveClassifier.get() ) {
+					"dev" -> "devlibs"
+					"sources" -> if ( this is RemapSourcesJarTask ) "libs" else "devlibs"
+					else -> "libs"
+				}
+			)
+		)
+	}
+
 	tasks.withType<Jar>().configureEach {
 		from( "LICENSE" ) {
 			rename { "${it}_$archivesBaseName" }
@@ -103,10 +124,8 @@ loom.runConfigs["client"].isIdeConfigGenerated = true
 loom.runConfigs["server"].isIdeConfigGenerated = true
 
 dependencies {
-	minecraft( "com.mojang:minecraft:$minecraft_version" )
-	mappings( loom.layered {
-		addLayer( quiltMappings.mappings( "org.quiltmc:quilt-mappings:$minecraft_version+build.$mappings:v2" ) )
-	})
+	minecraft( "com.mojang:minecraft:${version("minecraft")}" )
+	mappings( loom.layered { quilt( version("minecraft"), version("mappings") ) } )
 	runtimeOnly( include( project( path=":Core", configuration="namedElements" ) )!! )
 	runtimeOnly( include( project( path=":Flora", configuration="namedElements" ) )!! )
 
