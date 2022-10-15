69400c7c355c4e071a1cef8a4b2a22bcc9da5f66
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-10-15 16:56:32 +0200

    s

--- Flora/src/main/java/com/enderzombi102/diversity/flora/registry/ItemRegistry.java
+++ Flora/src/main/java/com/enderzombi102/diversity/flora/registry/ItemRegistry.java
@@ -3,21 +3,18 @@ package com.enderzombi102.diversity.flora.registry;
 import com.enderzombi102.diversity.flora.util.Const;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.Settings;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.NotNull;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.enderzombi102.diversity.core.registry.Registries.ITEMS;
 import static com.enderzombi102.diversity.flora.Flora.FLORAL_TAB;
-import static com.enderzombi102.diversity.flora.util.Const.getId;
 
 public final class ItemRegistry {
-	private static final Item CRYSTAL_GROUND = ITEMS.register( "crystal_ground", new BlockItem( BlockRegistry.get( "crystal_ground" ), settings() ) );
-	private static final Item CATAPLANT = ITEMS.register( "cataplant", new BlockItem( BlockRegistry.get( "cataplant" ), settings() ) );
+	private static final Item CATAPLANT = ITEMS.register( "cataplant", blockItem( "cataplant", settings() ) );
+	private static final Item CRYSTAL_GROUND = ITEMS.register( "crystal_ground", blockItem( "crystal_ground", settings() ) );
 
 	public static void init() { }
 
@@ -27,5 +24,9 @@ public final class ItemRegistry {
 
 	private static QuiltItemSettings settings() {
 		return new QuiltItemSettings().group( FLORAL_TAB );
+	}
+
+	private static @NotNull Item blockItem( @NotNull String id, @NotNull Settings settings ) {
+		return new BlockItem( BlockRegistry.get( id ), settings );
 	}
 }
